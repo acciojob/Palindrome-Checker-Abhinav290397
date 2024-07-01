4cb9b179c7471ab652a83a16332df7e1027219d8
Abhinav Mathur <142848301+Abhinav290397@users.noreply.github.com>
2024-07-02 02:43:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // complete the given function
 
 function palindrome(str){
-
+	let rev = "";
+	for(let i=str.length;i>=0;i--){
+		rev += str.charAt(i);
+	}
+	if(rev.equals(str))return true;
+	return false;
 }
 module.exports = palindrome
